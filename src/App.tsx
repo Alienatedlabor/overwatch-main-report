8c2fc94b2b6b12adf8100fe33f749eaa6bcf9a8a
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-06-26 16:49:37 -0400

    attempt player GET

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import overFastService from './services/overFastService';
 import './App.css';
 
 function App() {
-  return <>App</>;
+  const [searched, setSearched] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    overFastService.getPlayer(searched);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h1>Search for a player: </h1>
+          <input
+            value={searched}
+            onChange={(e) => setSearched(e.target.value)}
+            type="text"
+          />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default App;
---
+++ src/services/overFastService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const baseUrl = 'https://overfast-api.tekrop.fr/';
+
+const getPlayer = (playerName) => {
+  const request = axios.get(
+    `${baseUrl}players?name=${playerName}order_by=name%3Aasc&offset=0&limit=20`
+  );
+  return request.then((response) => console.log(response.data));
+};
+
+export default { getPlayer };
+
+//TODO: currently returning only empty array, figure out what's going on
