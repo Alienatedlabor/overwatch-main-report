732deac72521e9628572d209ae467d3832f64563
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-06-30 15:48:14 -0400

    add most played hero

--- src/services/overFastService.ts
+++ src/services/overFastService.ts
@@ -1,17 +1,41 @@
 import axios from 'axios';
+import { Hero, Player, PlayerInfo } from '../types/player';
 
 const baseUrl = 'https://overfast-api.tekrop.fr/';
 
-const getPlayer = (playerName: string) => {
-  const request = axios.get(
+const getPlayers = async (playerName: string): Promise<Player[]> => {
+  const response = await axios.get(
     `${baseUrl}players?name=${playerName}&order_by=name%3Aasc&offset=0&limit=20`
   );
-  return request.then((response) => response.data);
+  return response.data.results.map((player: any) => ({
+    name: player.name,
+    id: player.player_id,
+    isPrivate: player.privacy === 'private',
+  }));
 };
 
-const getPlayerInfo = (playerId: string) => {
-  const request = axios.get(`${baseUrl}players/${playerId}/stats/summary`);
-  return request.then((response) => response.data);
+const getPlayerInfo = async (playerId: string): Promise<PlayerInfo> => {
+  const response = await axios.get(
+    `${baseUrl}players/${playerId}/stats/summary`
+  );
+
+  let mostPlayedHero: Hero = 'ana';
+
+  for (const hero of Object.keys(response.data.heroes)) {
+    const currentHeroTimePlayed =
+      response.data.heroes[mostPlayedHero].time_played;
+    const nextHeroTimePlayed = response.data.heroes[hero].time_played;
+    if (nextHeroTimePlayed > currentHeroTimePlayed) {
+      mostPlayedHero = hero as Hero;
+    }
+  }
+
+  return {
+    gamesPlayed: response.data.general.games_played,
+    timePlayed: response.data.general.time_played,
+    winrate: response.data.general.winrate,
+    mostPlayedHero,
+  };
 };
 
-export default { getPlayer, getPlayerInfo };
+export default { getPlayers, getPlayerInfo };
