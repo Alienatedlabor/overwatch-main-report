84d2f2580751ddf5dadb6875ad083125f63118df
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-07-04 01:30:05 -0400

    add styles

--- src/components/DisplayPlayerInfo.tsx
+++ src/components/DisplayPlayerInfo.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-
 const DisplayPlayerInfo = ({ info }: any) => {
-  console.log(info);
   return (
-    <div>
+    <div className="px-16 py-8 rounded-md shadow-md my-4">
       <p>Total Games played: {info.gamesPlayed}</p>
       <p>Total time played: {info.timePlayed}</p>
       <p>Winrate: {info.winrate}%</p>
-      <p>This player's main is: {info.mostPlayedHero}</p>
+      <p className="font-bold">This player's main is: {info.mostPlayedHero}</p>
     </div>
   );
 };
