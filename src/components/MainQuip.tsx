f6726e2698537534b7ef13a4b1063627c0c6dce7
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-07-08 16:57:13 -0400

    finish quips

--- src/components/MainQuip.tsx
+++ src/components/MainQuip.tsx
@@ -88,37 +88,85 @@ const MainQuip = ({ mostPlayedHero }: MainQuipProps) => {
     case 'genji':
       return <p> I need healing</p>;
     case 'junker-queen':
-      return <p></p>;
+      return <p>Former roadhog mains are rejoicing after her buff.</p>;
+    case 'junkrat':
+      return <p>strategy? Throw enough bombs and hope something sticks. </p>;
+    case 'kiriko':
+      return (
+        <p>
+          Versatile and Well-rounded, until they teleport through the floor.
+        </p>
+      );
+    case 'lifeweaver':
+      return (
+        <p>
+          It took 4 patches of buffs, but they might not be throwing anymore?
+        </p>
+      );
+    case 'lucio':
+      return (
+        <p>
+          Booping and wall-riding while their team desperately needs healing,
+          yep, that's a redditor.
+        </p>
+      );
+    case 'mei':
+      return (
+        <p>Freezing enemies, or just freezing team progress? Nice wall!</p>
+      );
+    case 'mercy':
+      return <p>Probably boosted &#128521;</p>;
+    case 'moira':
+      return <p>35k damage 1k healing</p>;
+    case 'orisa':
+      return <p>giddy up &#128052; </p>;
+    case 'pharah':
+      return <p>justice rains from ableghhh &#128128; !</p>;
+    case 'rammatra':
+      return <p>grumpy zenyatta</p>;
+    case 'reaper':
+      return <p>goes to hot topic one time</p>;
+    case 'reinhardt':
+      return <p>W key and a dream</p>;
+    case 'roadhog':
+      return <p>look how they butchered my boy!</p>;
+    case 'sigma':
+      return <p>rock-solid</p>;
+    case 'sojourn':
+      return <p>pew pew pew pew</p>;
+    case 'soldier-76':
+      return <p>reformed call of duty player</p>;
+    case 'sombra':
+      return (
+        <p>
+          thinks they are playing splinter cell. spends more time running around
+          than shooting.
+        </p>
+      );
+    case 'symmetra':
+      return <p>&#129472;</p>;
+    case 'torbjorn':
+      return <p>he's got the good hammer</p>;
+    case 'tracer':
+      return (
+        <p>
+          They zip around so much, you'd think they were trying to outrun their
+          commitment issues.
+        </p>
+      );
+    case 'widowmaker':
+      return <p>either the best player on the team, or the worst</p>;
+    case 'winston':
+      return <p>&#129421; winton &#129421;</p>;
+    case 'wrecking-ball':
+      return <p>a rodent in a maze</p>;
+    case 'zarya':
+      return <p>when you want to be a dps</p>;
+    case 'zenyatta':
+      return <p>when you want to be a dps</p>;
     default:
       return null;
   }
 };
 
 export default MainQuip;
-
-//   | 'junker-queen'
-//   | 'junkrat'
-//   | 'kiriko'
-//   | 'lifeweaver'
-//   | 'lucio'
-//   | 'mei'
-//   | 'mercy'
-//   | 'moira'
-//   | 'orisa'
-//   | 'pharah'
-//   | 'rammatra'
-//   | 'reaper'
-//   | 'reinhardt'
-//   | 'roadhog'
-//   | 'sigma'
-//   | 'sojourn'
-//   | 'solder-76'
-//   | 'sombra'
-//   | 'symmetra'
-//   | 'torbjorn'
-//   | 'tracer'
-//   | 'widowmaker'
-//   | 'winston'
-//   | 'wrecking-ball'
-//   | 'zarya'
-//   | 'zenyatta';
